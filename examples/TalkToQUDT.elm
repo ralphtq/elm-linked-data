053d48751f3d8ec4ffa11405efc14653b0615ef8
ralphtq <rhodgson@topquadrant.com>
2021-12-27 19:39:23 -0500

    Experiments with QUDT requests - inc2

--- examples/TalkToQUDT.elm
+++ examples/TalkToQUDT.elm
@@ -14,19 +14,46 @@ import RDF
 import RDF.Decode
 import RDF.JSON
 import Return exposing (Return)
+import Url
 
 
 
 -- Config
 
 
-publicUrl : String
-publicUrl =
-    "http://www.qudt.org/fuseki/qudt/query"
+qudtQueryEndpointURL : String
+qudtQueryEndpointURL = "http://www.qudt.org/fuseki/qudt/query"
 
-someClasses = "query=prefix+owl%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0Aprefix+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0A%0ASELECT+DISTINCT+%3Fclass+%3Flabel+%3Fdescription%0AWHERE+%7B%0A++%3Fclass+a+owl%3AClass.%0A++OPTIONAL+%7B+%3Fclass+rdfs%3Alabel+%3Flabel%7D%0A++OPTIONAL+%7B+%3Fclass+rdfs%3Acomment+%3Fdescription%7D%0A%7D%0ALIMIT+25"
+prefixForOWL  = Url.percentEncode "prefix owl:  <http://www.w3.org/2002/07/owl#>"
+prefixForRDFS = Url.percentEncode "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
 
-someTriples = "query=prefix+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0Aprefix+owl%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0A%0ASELECT+%3Fsubject+%3Fpredicate+%3Fobject%0AWHERE+%7B%0A++%3Fsubject+%3Fpredicate+%3Fobject%0A%7D%0ALIMIT+25"
+encodedPrefixes = prefixForOWL ++ prefixForRDFS
+
+queryQUDTclasses = 
+ """
+ SELECT ?class ?label ?description
+ WHERE {
+     ?class a owl:Class .
+     OPTIONAL {?class rdfs:label ?label}
+     OPTIONAL {?class rdfs:comment ?description}
+ }
+ """
+
+queryQUDTtriples = 
+ """
+ SELECT ?subject ?predicate ?object
+ WHERE {
+   ?subject ?predicate ?object
+ }
+ """
+
+
+sparqlQuery : String -> Int -> String
+sparqlQuery query max = query ++ "LIMIT" ++ String.fromInt max
+
+queryClasses = encodedPrefixes ++ ( String.replace "%20" "+" (Url.percentEncode (sparqlQuery queryQUDTclasses 10)))
+
+queryTriples = encodedPrefixes ++ ( String.replace "%20" "+" (Url.percentEncode (sparqlQuery queryQUDTtriples 10)))
 
 -- "http://localhost:4000/public"
 -- "https://openengiadina.net/public"
@@ -41,7 +68,7 @@ main =
         , subscriptions = subscriptions
         }
 
-
+type alias SPARQLquery = String
 
 -- MODEL
 
@@ -49,6 +76,7 @@ main =
 type alias Model =
     { graph : RDF.Graph
     , content : String
+    , query : String
     }
 
 
@@ -56,26 +84,23 @@ init : {} -> Return Msg Model
 init flags =
     { graph = RDF.fromList []
     , content = ""
+    , query = ""
     }
         |> Return.singleton
-        |> Return.command getNotes
+        -- |> Return.command (queryEndpoint queryClasses)
 
 
-getNotes : Cmd Msg
-getNotes =
-    Http.request -- get
+queryEndpoint : SPARQLquery -> Cmd Msg
+queryEndpoint query =
+    Http.request
         { method = "POST"
         , headers = [ Http.header "Authorization" "Basic" ]
         , url = "http://www.qudt.org/fuseki/qudt/query"
-        , body = Http.stringBody "application/x-www-form-urlencoded" someClasses  -- (encodeNote content)
-        , expect = Http.expectJson GotQUDT (JD.list JD.string)
+        , body = Http.stringBody "application/x-www-form-urlencoded" ("query=" ++ query )
+        , expect = Http.expectString GotQueryResponse
         , timeout = Nothing
         , tracker = Nothing
         }
-        -- { url = publicUrl
-        -- , expect = Http.expectJson Receive RDF.JSON.decoder
-        -- }
-
 
 
 -- UPDATE
@@ -84,9 +109,10 @@ getNotes =
 type Msg
     = Receive (Result Http.Error RDF.Graph)
     | UpdateContent String
-    | Post
+    | QueryClasses
+    | QueryTriples
     | Refresh
-    | GotQUDT (Result Http.Error (List String))
+    | GotQueryResponse (Result Http.Error String)
 
 
 update : Msg -> Model -> Return Msg Model
@@ -105,18 +131,25 @@ update msg model =
             { model | content = s }
                 |> Return.singleton
 
-        Post ->
-            model
+        QueryClasses ->
+            { model | query = queryClasses }
                 |> Return.singleton
-                |> Return.command (postNote model.content)
+                |> Return.command (queryEndpoint queryClasses)
 
+        QueryTriples ->
+            { model | query = queryTriples }
+                |> Return.singleton
+                |> Return.command (queryEndpoint queryTriples)
         Refresh ->
             model
                 |> Return.singleton
-                |> Return.command getNotes
-        GotQUDT c ->
-             model
-                |> Return.singleton
+                |> Return.command (queryEndpoint queryClasses)
+
+        GotQueryResponse result -> 
+                case result of 
+                  Ok response ->  
+                    { model | content = response } |> Return.singleton
+                  Err _  -> model |> Return.singleton
 
 
 
@@ -127,140 +160,22 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
-
-
--- ActivityStreams Notes
-
-
-activityStreams : String -> RDF.IRI
-activityStreams =
-    RDF.namespace "https://www.w3.org/ns/activitystreams#"
-
-
-type alias Note =
-    { iri : RDF.IRI
-    , content : RDF.Literal
-    , context : Maybe RDF.IRI
-    }
-
-
-noteDecoder : RDF.Decode.Decoder Note
-noteDecoder =
-    RDF.Decode.succeed Note
-        |> RDF.Decode.ignore (RDF.Decode.ensureType <| activityStreams "Note")
-        |> RDF.Decode.apply RDF.Decode.iriDecoder
-        |> RDF.Decode.apply
-            (RDF.Decode.objectsDecoder (activityStreams "content" |> RDF.predicateIRI)
-                RDF.Decode.literalDecoder
-                |> RDF.Decode.first
-            )
-        |> RDF.Decode.apply
-            (RDF.Decode.objectsDecoder (activityStreams "context" |> RDF.predicateIRI)
-                RDF.Decode.iriDecoder
-                |> RDF.Decode.first
-                |> RDF.Decode.maybe
-            )
-
-
-getNotesFromGraph : RDF.Graph -> List Note
-getNotesFromGraph graph =
-    graph
-        |> RDF.Decode.decodeAll noteDecoder
-
-
-
 -- VIEW
 
 
-noteView : Note -> H.Html Msg
-noteView note =
+showResults : Model -> H.Html Msg
+showResults model =
     H.div []
-        [ H.a
-            [ HA.href (note |> .iri)
-            , HA.target "_blank"
-            ]
-            [ H.h3 [] [ note |> .iri |> H.text ] ]
-        , H.pre [] [ note |> .content |> .value |> H.text ]
-        ]
-
-
-notesView : RDF.Graph -> H.Html Msg
-notesView graph =
-    let
-        notes =
-            getNotesFromGraph graph
-    in
-    H.div [] (notes |> List.map noteView)
-
-
-postNote : String -> Cmd Msg
-postNote content =
-    Http.request
-        { method = "POST"
-        , headers = [ Http.header "Authorization" "Basic" ]
-        , url = "http://www.qudt.org/fuseki/qudt/query"
-        , body = Http.stringBody "application/rdf+json" content -- (encodeNote content)
-        , expect = Http.expectWhatever (always Refresh)
-        , timeout = Nothing
-        , tracker = Nothing
-        }
-
-
-encodeNote : String -> String
-encodeNote content =
-    let
-        object_id =
-            "object"
-                |> RDF.blankNode
-
-        activity_id =
-            "activity"
-                |> RDF.blankNode
-                |> RDF.subjectBlankNode
-    in
-    RDF.empty
-        -- activity
-        |> RDF.addTriple
-            (RDF.Triple activity_id RDF.type_ (activityStreams "Create" |> RDF.objectIRI))
-        |> RDF.addTriple
-            (RDF.Triple activity_id (activityStreams "to" |> RDF.predicateIRI) ("http://localhost:4000/users/alice" |> RDF.objectIRI))
-        |> RDF.addTriple
-            (RDF.Triple activity_id (activityStreams "to" |> RDF.predicateIRI) (activityStreams "Public" |> RDF.objectIRI))
-        |> RDF.addTriple
-            (RDF.Triple activity_id (activityStreams "object" |> RDF.predicateIRI) (object_id |> RDF.objectBlankNode))
-        -- object
-        |> RDF.addTriple
-            (RDF.Triple (object_id |> RDF.subjectBlankNode)
-                RDF.type_
-                (activityStreams "Note" |> RDF.objectIRI)
-            )
-        |> RDF.addTriple
-            (RDF.Triple (object_id |> RDF.subjectBlankNode)
-                (activityStreams "content" |> RDF.predicateIRI)
-                (RDF.literal content (RDF.xsd "string") Nothing
-                    |> RDF.objectLiteral
-                )
-            )
-        |> RDF.JSON.encode
-        |> JE.encode 2
-
-
-composeNote : String -> H.Html Msg
-composeNote content =
-    H.div []
-        [ H.input
-            [ HA.value content
-            , HE.onInput UpdateContent
-            ]
-            []
-        , H.button [ HE.onClick Post ] [ H.text "Post" ]
-        , H.p []
-            [ "An encoded activity with the note looks like this (encoded as RDF/JSON):"
+        [ H.p []
+            [ model.query
+                |> String.replace "+" " "
+                |> Url.percentDecode
+                |> Maybe.withDefault model.query
                 |> H.text
             ]
         , H.code []
             [ H.pre []
-                [ encodeNote content
+                [   model.content
                     |> H.text
                 ]
             ]
@@ -269,18 +184,20 @@ composeNote content =
 
 view : Model -> B.Document Msg
 view model =
-    { title = "Talk to QUDT"
+    { title = "Talk to QUDT (v0.1)"
     , body =
         [ H.header []
-            [ H.h1 [] [ H.text "Talk to QUDT" ]
-            , H.p [] [ H.text "This is a small example app that queries QUDT" ]
+            [ H.h1 [] [ H.text "Talk to QUDT (v0.1)" ]
+            , H.p [] [ H.text "Query the QUDT SPARQL Endpoint" ]
             ]
         , H.main_
             []
-            [ H.h2 [] [ H.text "Query" ]
-            , composeNote model.content
-            -- , H.h2 [] [ H.text "Notes" ]
-            -- , notesView model.graph
+            [ H.h2 []
+            [ "SPARQL Query:"
+                |> H.text]
+            , H.button [ HE.onClick QueryClasses ] [ H.text "Run Classes Query" ]
+            , H.button [ HE.onClick QueryTriples ] [ H.text "Run Triples Query" ]
+            , showResults model
             ]
-        ]
+         ] 
     }
